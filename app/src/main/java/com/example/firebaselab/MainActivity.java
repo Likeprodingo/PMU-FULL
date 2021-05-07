2961cd632b180912df00feb639f36aa7a596840f
Likeprodingo <alshiba1@mail.ru>
2021-05-07 18:11:38 +0300

    Main Activity SQL

--- app/src/main/java/com/example/firebaselab/MainActivity.java
+++ app/src/main/java/com/example/firebaselab/MainActivity.java
@@ -44,7 +44,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements PostsAdapter.OnPostListener{
 
     public static boolean sqlite = true;
     public static boolean admin = false;
@@ -63,7 +63,164 @@ public class MainActivity extends AppCompatActivity{
     public List<Post> posts;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Context context = getApplicationContext();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        posts = new ArrayList<>();
+
+        postsList = findViewById(R.id.rv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        postsList.setLayoutManager(layoutManager);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("database")) {
+            String database = intent.getStringExtra("database");
+            if (database.equals("firebase"))
+                sqlite = false;
+            else
+                sqlite = true;
+        }
+
+
+
+        if (sqlite) {
+
+            db = new PostsSQLite.PostsReaderDbHelper(context).getWritableDatabase();
+            try {
+                loadPostsFromSQlite();
+            } catch (MalformedURLException | ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (intent.hasExtra("update")) {
+                try {
+                    loadPostsFromSQlite();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (intent.hasExtra("position")) {
+                int pos = intent.getIntExtra("position", -1);
+                for(Post p : posts) {
+                    Log.d("post before:" , p.title);
+                }
+                Log.d("size before:", posts.size() + "");
+                posts.remove(pos);
+                db.execSQL("DELETE FROM posts");
+                Log.d("size after:", posts.size() + "");
+                Collections.reverse(posts);
+                for(Post p : posts) {
+                    ContentValues values = new ContentValues();
+                    values.put(PostsSQLite.PostsEntry.COLUMN_NAME_TITLE, p.title);
+                    if (p.getDescription() != null)
+                        values.put(PostsSQLite.PostsEntry.COLUMN_NAME_DESCRIPTION, p.description);
+                    if (p.getImagePath() != null)
+                        values.put(PostsSQLite.PostsEntry.COLUMN_NAME_IMAGE_PATH, p.getImagePath());
+                    long newRowId = db.insert(PostsSQLite.PostsEntry.TABLE_NAME, null, values);
+                }
+                posts.clear();
+                try {
+                    loadPostsFromSQlite();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
+
+    @Override
+    public void onPostClick(int position) {
+        Post post = posts.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("position", position);
+        bundle.putString("title", post.title);
+        bundle.putString("description", post.description);
+        if (post.getImagePath() != null)
+            bundle.putString("imagePath", post.imagePath);
+        if (post.getSongPath() != null)
+            bundle.putString("songPath", post.songPath);
+        if (!post.getImageUrl().isEmpty()) {
+            bundle.putString("imageUrl", post.imageUrl);
+        }
+        ShowPost postFragment = new ShowPost();
+        postFragment.setArguments(bundle);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.host_activity, postFragment);
+        fragmentTransaction.commit();
+        fab.setVisibility(View.INVISIBLE);
+    }
+
+    void reloadPostsFromSQLite() {
+        posts.clear();
+        String[] projection = {
+                PostsSQLite.PostsEntry.COLUMN_NAME_TITLE,
+                PostsSQLite.PostsEntry.COLUMN_NAME_DESCRIPTION,
+                PostsSQLite.PostsEntry.COLUMN_NAME_IMAGE_PATH
+        };
+
+        Cursor cursor = db.query(PostsSQLite.PostsEntry.TABLE_NAME, projection, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            Post p = new Post();
+            p.title = cursor.getString(0);
+            p.description = cursor.getString(1);
+            if (cursor.getString(2) != null) {
+                p.setImagePath(cursor.getString(2));
+            }
+            posts.add(p);
+        }
+        cursor.close();
+    }
+    void loadPostsFromSQlite() throws MalformedURLException, ExecutionException, InterruptedException {
+        List<Post> postsFromSQL = new ArrayList<>();
+        String[] projection = {
+                PostsSQLite.PostsEntry.COLUMN_NAME_TITLE,
+                PostsSQLite.PostsEntry.COLUMN_NAME_DESCRIPTION,
+                PostsSQLite.PostsEntry.COLUMN_NAME_IMAGE_PATH
+        };
+
+        Cursor cursor = db.query(PostsSQLite.PostsEntry.TABLE_NAME, projection, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            Post p = new Post();
+            p.title = cursor.getString(0);
+            p.description = cursor.getString(1);
+            if (cursor.getString(2) != null) {
+                p.setImagePath(cursor.getString(2));
+            }
+            postsFromSQL.add(0, p);
+        }
+        cursor.close();
+    }
+
+
+    List collectPosts(Map<String, Object> postsFromDb) {
+        List<Post> ps = new ArrayList<>();
+        if (postsFromDb != null) {
+            for (Map.Entry<String, Object> entry : postsFromDb.entrySet()) {
+                Post p = new Post();
+
+                Map singlePost = (Map) entry.getValue();
+                String title = singlePost.get("title").toString();
+                String description = null;
+                String imagePath = null;
+                if (singlePost.get("description") != null)
+                    description = singlePost.get("description").toString();
+                if (singlePost.get("imagePath") != null)
+                    imagePath = singlePost.get("imagePath").toString();
+                p.title = title;
+                p.description = description;
+                p.imagePath = imagePath;
+                p.setNumber(singlePost.get("number").toString());
+                //posts.add(p);
+                ps.add(p);
+            }
+        }
+        Collections.sort(ps);
+        Collections.reverse(ps);
+        return ps;
+    }
+
 }
